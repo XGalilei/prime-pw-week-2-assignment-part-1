d7f3d6e5a18d51e366c5471221dfe71925f892a4
XGalilei <xgalilei@DESKTOP-K04H92P.localdomain>
2021-07-21 13:36:32 -0500

    Continue work on stretch goals

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -84,6 +84,7 @@ switch (pets) {
     break;
   default:
 }
+console.log(mostPets);
 
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
 adventurous ? console.log("Adventures are great!") : console.log("How about we stay home?");
